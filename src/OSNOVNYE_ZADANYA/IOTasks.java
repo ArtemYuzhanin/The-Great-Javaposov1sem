863b94a4a1509ada518860c60e29362fab66d045
Артемий <st076453@student.spbu.ru>
2020-06-05 23:18:59 +0300

    ДА СкОЛьКо МОЖнО уЖ-АНТРАКТИЩЩЕ!!!

--- src/OSNOVNYE_ZADANYA/IOTasks.java
+++ src/OSNOVNYE_ZADANYA/IOTasks.java
@@ -9,21 +9,13 @@ import java.util.Scanner;
 
 public class IOTasks {
     public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
-        String name = in.next();
-        System.out.println("Введите кол-во строк");
-        int num = in.nextInt();
-        System.out.println("Вводите строки");
-        String[] massive= new String[num];
-        for (int i = 0;i<massive.length;i++) {
-            massive[i] = in.next();
-        }
-        for (String line : massive) {
-            System.out.println(line);
-        }
-        WriteThatDown(name, massive);
-        in.close();
-
+        Scanner pin = new Scanner(System.in);
+        String name = pin.next();
+        String nametwo = pin.next();
+        File a = new File("src\\OSNOVNYE_ZADANYA\\"+name);
+        File b = new File("src\\OSNOVNYE_ZADANYA\\"+nametwo);
+        FixItFelix(a,b);
+        pin.close();
     }
 
     public static int TextSumm(File f) throws Exception {
@@ -45,5 +37,55 @@ public class IOTasks {
             ps1.println(line);
         }
         ps1.close();
+    }
+
+    public static void FixItFelix(File a, File b) throws Exception{
+        Scanner in = new Scanner(a, "utf-8");
+        String w = in.next();
+        boolean p = true;
+        char last = w.charAt(w.length() - 1);;
+        PrintStream ps1 = new PrintStream(b);
+        while (in.hasNext()) {
+            if (w.charAt(0) != '-') {
+                switch (last) {
+                    case '!':
+                        p = true;
+                        break;
+                    case '?':
+                        p = true;
+                        break;
+                    case '.':
+                        p = true;
+                        break;
+                    default:
+                }
+                if (p) {
+                    w = w.substring(0, 1).toUpperCase() + w.substring(1).toLowerCase();
+                }
+                else
+                    w = w.substring(0, w.length() - 1).toLowerCase() + w.substring(w.length()-1);
+            }
+            p = false;
+            ps1.print(w+' ');
+            System.out.println(w+' ');;
+            last = w.charAt(w.length() - 1);
+            w = in.next();
+        }
+        switch (last) {
+            case '!':
+                ps1.print(w.substring(0, 1).toUpperCase() + w.substring(1, w.length() - 1).toLowerCase() + w.substring(w.length()-1));
+                break;
+            case '?':
+                ps1.print(w.substring(0, 1).toUpperCase() + w.substring(1, w.length() - 1).toLowerCase() + w.substring(w.length()-1));
+                break;
+            case '.':
+                ps1.print(w.substring(0, 1).toUpperCase() + w.substring(1, w.length() - 1).toLowerCase() + w.substring(w.length()-1));
+                break;
+            default:
+                ps1.print(w.substring(0, w.length() - 1).toLowerCase() + w.substring(w.length()-1));
+        }
+        ps1.close();
+        in.close();
+
     }
 }
